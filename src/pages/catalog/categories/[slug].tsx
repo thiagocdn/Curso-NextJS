1a15995fb2a852d45fd2164c54c7d78713b2b9c4
Thiago Cedran <thiagocdn@gmail.com>
2020-12-27 13:36:53 -0300

    Static Pages on Request

--- src/pages/catalog/categories/[slug].tsx
+++ src/pages/catalog/categories/[slug].tsx
@@ -12,6 +12,11 @@ interface CategoryProps {
 
 export default function Category({ products }: CategoryProps) {
   const router = useRouter();
+
+  if(router.isFallback) {
+    return <p>Carregando...</p>
+  };
+
   return(
     <div>
       <h1>{router.query.slug}</h1>
@@ -43,7 +48,7 @@ export const getStaticPaths: GetStaticPaths = async () => {
 
   return {
     paths,
-    fallback: false,
+    fallback: true,
   }
 
 };
