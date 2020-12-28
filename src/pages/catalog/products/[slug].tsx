1dca1789031dc8890640d39879e24f4c819b7804
Thiago Cedran <thiagocdn@gmail.com>
2020-12-27 23:15:32 -0300

    Added paths

--- src/pages/catalog/products/[slug].tsx
+++ src/pages/catalog/products/[slug].tsx
@@ -3,7 +3,7 @@ import { useRouter } from 'next/router';
 import { useState } from 'react';
 
 const AddToCartModal = dynamic(
-  () => import('../../../components/AddToCartModal'),
+  () => import('@/components/AddToCartModal'),
   {
     loading: () => <p>Loading...</p>,
     ssr: false,
